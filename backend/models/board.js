865d1f528f0f0a9676dbc0d13a2ea9565955c0f0
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-17 13:48:08 -0500

    Se agrega el login y las tareas

--- backend/models/board.js
+++ backend/models/board.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const moment = require("moment");
+
 
 const boardSchema =  new mongoose.Schema({
     userId: {type: mongoose.Schema.ObjectId, ref: "user"},
@@ -10,16 +9,6 @@ const boardSchema =  new mongoose.Schema({
     imageUrl: String,
     date: {type: Date, default: Date.now},
 });
-
-userSchema.methods.generateJWT =function(){
-    return jwt.sign({
-        _id: this._id.Date,
-        name: this.name,
-        iat: moment().unix(),
-    },
-    process.env.SECRET_KEY_JWT
-    );
-};
 
 const board = mongoose.model("board", boardSchema);
 
